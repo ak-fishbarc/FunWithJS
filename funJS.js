c6de83559269f2dfa6aebb23cfc1ba3a969f4eba
ak-fishbarc <rybarczyk.ak@gmail.com>
2023-02-09 14:25:49 +0000

    Current work: Building Construction

--- funJS.js
+++ funJS.js
@@ -18,7 +18,7 @@ const castle = {
   gold: 100
 };
 
-
+/* Create player */
 function createPlayer(name)
 {
   const player = {
@@ -32,29 +32,63 @@ function createPlayer(name)
 };
 
 player = createPlayer("Player1");
+// Later might be refactored together with hideStats to create one function
+// to catch them all.
+function removeOptions(entity)
+{
+  console.log("Here");
+  constrBuild = document.getElementById("constrBuild");
+  document.getElementById(entity.id).removeChild(constrBuild);
+  document.getElementById(entity.id).onclick = function() { constrOptions(entity) };
+}
 
+function createCastle()
+{
+  console.log("Nothing for now");
+}
+// Create options for building creation
+function constrOptions(entity)
+{
+  console.log(entity)
+  constrBuild = document.createElement("div");
+  constrBuild.id = "constrBuild";
+  document.getElementById(entity.id).appendChild(constrBuild);
+  document.getElementById(entity.id).onclick = function() { removeOptions(entity) };
+
+  // Temporarily Here
+  castleIcon = document.createElement("div");
+  castleIcon.id = "castleIcon";
+  document.getElementById("constrBuild").appendChild(castleIcon);
+  document.getElementById("castleIcon").onclick = function() { createCastle() };
+}
+
+
+/* Create militia. Add count of militia to player's stats */
 function createMilitia(player)
 {
     player.militia_count += 1;
 }
 
+/* Add "button" to create militia, when player clicks on the castle */
 function castleBuildsFun(entity, player)
 {
   castleBuilds = document.createElement("div");
   castleBuilds.innerHTML = "Build Militia";
   castleBuilds.id = "castleBuilds";
   document.getElementById(entity.id).appendChild(castleBuilds);
-  castleBuilds.onclick = function () { createMilitia(player) };
+  castleBuilds.onclick = function() { createMilitia(player) };
 }
 
+// Hide stats for buildings.
 function hideStats(entity)
 {
   entityStats = document.getElementById(entity.name+"Stats");
   document.getElementById(entity.name).removeChild(entityStats);
-  document.getElementById(entity.name).onclick = function () { showStats(entity, player); };
-
+  document.getElementById(entity.name).onclick = function() { showStats(entity, player); };
 }
 
+/* Show stats for buildings, when clicked on. Creates element in HTML and
+ shows information about the building.*/
 function showStats(entity, player)
 {
   entityStats = document.createElement("div")
@@ -62,7 +96,9 @@ function showStats(entity, player)
   "Food: " + entity.food;
   entityStats.id = entity.name+"Stats";
   document.getElementById(entity.name).appendChild(entityStats);
-  document.getElementById(entity.name).onclick = function () { hideStats(entity); };
+  document.getElementById(entity.name).onclick = function() { hideStats(entity); };
+
+  // Extra function for castles. Allowing castles to build militia.
   if(entityStats.id == "castleStats")
   {
     castleBuildsFun(entityStats, player);
