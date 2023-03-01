0b5a965212e9e9ec716832592df7fbed52bf2ba3
ak-fishbarc <rybarczyk.ak@gmail.com>
2023-03-01 11:19:31 +0000

    Refactored most of building function. Added farm building

--- funJS.js
+++ funJS.js
@@ -20,7 +20,7 @@ function buildCastle(identity)
 {
   const castle_building =
   {
-    const_name: "castle",
+    const_name: "Castle",
     identity: identity,
     hp: 1000,
     food: 10,
@@ -35,7 +35,7 @@ function buildFarm(identity)
 {
   const farm_building =
   {
-    const_name: "farm",
+    const_name: "Farm",
     identity: identity,
     hp: 100,
     food: 5,
@@ -46,36 +46,25 @@ function buildFarm(identity)
   return farm_building;
 };
 
+function createIdentity(const_name)
+{
+  return identity = const_name + buildings.length.toString();
+};
+
 player = createPlayer("Player1");
 start_castle = buildCastle("start_castle");
 start_farm = buildFarm("start_farm");
-
 
 players.push(player);
 
 buildings.push(start_castle);
 buildings.push(start_farm);
 
-function removeOptions(entity)
-{
-  construct_building = document.getElementById("construct_building");
-  document.getElementById(entity.id).removeChild(construct_building);
-  document.getElementById(entity.id).onclick = function() { constructionOptions(entity); };
-}
-
 function attachEvents(entity)
 {
   document.getElementById(entity.identity).innerHTML = entity.const_name;
   document.getElementById(entity.identity).onclick = function() { showStats(entity, player); };
 }
-
-function createIdentity(const_name)
-{
-  return identity = const_name + buildings.length.toString();
-}
-
-/// For now, all the building function are done for castles, just as a prototype.
-/// As initial function get sorted out, this will change.
 
 function checkPlace(entity)
 {
@@ -88,68 +77,111 @@ function checkPlace(entity)
     // This is why style properties of those objects need this extra line of code.
     html_element = window.getComputedStyle(document.getElementById(buildings[i].identity))
     // Check for collision.
-    if(parseInt(es.left) + parseInt(es.width) < parseInt(html_element.left) || parseInt(es.top) + parseInt(es.height) < parseInt(html_element.top)
-      || parseInt(html_element.left) + parseInt(html_element.width) < parseInt(es.left) || parseInt(html_element.top) + parseInt(html_element.height) < parseInt(es.top))
+    // First is to check if the building is not colliding with sidebar.
+    if(parseInt(es.left) < 125)
     {
-      // This comparision is due to looping through all the buildings on the map.
-      // If there is a collision with any building it must return red.
-      // This part is to be refactored but I don't have any more time today to do that.
-      if(result != "Red" || result == "")
-      {
-        result = "Green";
-      }
+      return "Red";
+    }
+    if(parseInt(es.left) + parseInt(es.width) < parseInt(html_element.left) ||
+    parseInt(es.top) + parseInt(es.height) < parseInt(html_element.top) ||
+    parseInt(html_element.left) + parseInt(html_element.width) < parseInt(es.left) ||
+    parseInt(html_element.top) + parseInt(html_element.height) < parseInt(es.top))
+    {
+      result = "Green";
     }
     else
     {
-      result = "Red";
+      // If any of the buildings is in the way, stop and return red.
+      return "Red";
     }
   }
   return result;
 }
-
-function putCastle(event, castle)
+//////////////////////////////////////////////////////////////////////////////////
+// Switch case is in putBuilding andd positionBuilding for now; Until I have time
+// to write better solution.
+//////////////////////////////////////////////////////////////////////////////////
+function putBuilding(event, building, building_name)
 {
   document.onmousemove = "";
   document.onclick = "";
-  castle.style.position = "fixed";
-  castle.style.backgroundColor = "grey";
-  document.body.appendChild(castle);
-  new_castle = buildCastle(castle.id);
-  attachEvents(new_castle);
+
+  building.style.position = "fixed";
+  switch(building_name)
+  {
+    case "castle":
+    building.style.backgroundColor = "grey";
+    new_building = buildCastle(building.id);
+    break;
+
+    case "farm":
+    building.style.backgroundColor = "blue";
+    new_building = buildFarm(building.id);
+    break;
+  }
+
+  // Building refers to HTML element, new_building refers to JS const.
+  document.body.appendChild(building);
+  attachEvents(new_building);
+  buildings.push(new_building);
 }
 
-function positionCastle(event, castle)
+function positionBuilding(event, building, building_name)
 {
-
-  castle.style.height = "100px";
-  castle.style.width = "80px";
-  castle.style.top = event.clientY + "px";
-  castle.style.left = event.clientX + "px";
-  if(checkPlace(castle) == "Green")
+  switch(building_name)
   {
-    document.onclick = function(event) { putCastle(event, castle); };
+    case "castle":
+    building.style.height = "100px";
+    building.style.width = "80px";
+    break;
+
+    case "farm":
+    building.style.height = "50px";
+    building.style.width = "50px";
+    break;
+  }
+
+  building.style.top = event.clientY + "px";
+  building.style.left = event.clientX + "px";
+
+  if(checkPlace(building) == "Green")
+  {
+    document.onclick = function(event) { putBuilding(event, building, building_name); };
   }
   else
   {
     document.onclick = "";
   }
-  console.log(checkPlace(castle));
+  console.log(building_name);
+  console.log(checkPlace(building));
 }
 
-function moveCastle(event, castle, entity)
+function moveBuilding(event, entity, building, building_name)
 {
-  document.onmousemove = function(event) { positionCastle(event, castle); };
+  document.onmousemove = function(event) { positionBuilding(event, building, building_name); };
   entity.onmouseleave = "";
 }
-
-function createCastle(event, entity)
+///////////////////////////////////////////////////////////////////////////////////
+// Argument "entity" takes value of "construction" HTML element, to set and delete
+// events related to this element.
+// Argument "building_name" takes string to create Unique Identifier for the
+// building. This should be related to the type of building: "castle" for castle,
+// "farm" for farm etc. createIdentity will return, e.g. "castle2" as new id.
+///////////////////////////////////////////////////////////////////////////////////
+function createBuilding(event, entity, building_name)
 {
-  new_castle = document.createElement("div");
-  new_identity = createIdentity("castle");
-  new_castle.id = new_identity;
-  entity.onmouseleave = function(event) { moveCastle(event, new_castle, entity); };
+  new_building = document.createElement("div");
+  new_identity = createIdentity(building_name);
+  new_building.id = new_identity;
+  entity.onmouseleave = function(event) { moveBuilding(event, entity, new_building, building_name); };
 }
 
+function removeOptions(entity)
+{
+  construct_building = document.getElementById("construct_building");
+  document.getElementById(entity.id).removeChild(construct_building);
+  document.getElementById(entity.id).onclick = function() { constructionOptions(entity); };
+}
 // Building construction options
 function constructionOptions(entity)
 {
@@ -163,7 +195,13 @@ function constructionOptions(entity)
   castle_icon.id = "castle_icon";
 
   document.getElementById("construct_building").appendChild(castle_icon);
-  document.getElementById("castle_icon").onclick = function() { createCastle(event, entity); };
+  document.getElementById("castle_icon").onclick = function() { createBuilding(event, entity, "castle"); };
+
+  farm_icon = document.createElement("div");
+  farm_icon.id = "farm_icon";
+
+  document.getElementById("construct_building").appendChild(farm_icon);
+  document.getElementById("farm_icon").onclick = function() { createBuilding(event, entity, "farm"); };
 }
 
 
@@ -206,7 +244,7 @@ function showStats(entity, player)
   document.getElementById(entity.identity).onclick = function() { hideStats(entity); };
 
   // Function for castles to allow them to build units.
-  if(entity_stats.id.search('castle'))
+  if(entity.const_name == "Castle")
   {
     castleBuildUnits(entity_stats, player);
   }
